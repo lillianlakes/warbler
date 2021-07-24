33ce5c8d4d17194f01d233cb79d49ed7fe61391a
jragni <jagni001@ucr.edu>
2021-07-23 17:38:34 -0700

    FEAT:Debugging

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
-
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
 database_url = database_url.replace('postgres://', 'postgresql://')
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-
+# added line below
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 toolbar = DebugToolbarExtension(app)
 
