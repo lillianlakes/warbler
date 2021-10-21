733ac8ae7886815536e38f6f1765405ff17da1b1
lillianlakes <lillian.lakes@gmail.com>
2021-10-21 15:35:03 -0700

    preparing to deploy to heroku

--- app.py
+++ app.py
@@ -8,28 +8,26 @@ from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
-database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
-database_url = database_url.replace('postgres://', 'postgresql://')
 
 app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-#app.config['SQLALCHEMY_DATABASE_URI'] = (
-#    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+   os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+database_url = database_url.replace('postgres://', 'postgresql://')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-## for heroku:
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
 # added line below
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-
 
 ##############################################################################
 # User signup/login/logout
