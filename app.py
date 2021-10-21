a5d2e29c6e5f5b54a99d0fcb1df3ef0adcf09143
lillianlakes <lillian.lakes@gmail.com>
2021-10-21 10:24:01 -0700

    updates config file

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 ## for heroku:
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
