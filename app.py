1e0c5984214bf2cf38d2425950af74e78b754858
lillianlakes <lillian.lakes@gmail.com>
2021-10-21 09:48:45 -0700

    adds a comment about config in app.py

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = (
 #    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
 
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+## for heroku:
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
 # added line below
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 toolbar = DebugToolbarExtension(app)
